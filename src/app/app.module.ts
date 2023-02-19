b6cad675b808d8a3643f07d805e57468343b879c
sarra.hammami <sarra.hammami456@gmail.com>
2023-02-19 15:49:52 +0100

    secondcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
+import { Page2Component } from './about/page2.component';
+import { PartnershipComponent } from './partnership/partnership.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
     Page2Component,
+    PartnershipComponent,
+ 
   ],
   imports: [
     BrowserModule,
